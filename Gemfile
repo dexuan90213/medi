51bfdff3bdae88a618226787f1e04e5a358f87fc
Eden Yang <dexuan90213@gmail.com>
2020-03-01 18:38:44 +0800

    add story url friendly_id

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 gem 'devise', '~> 4.7', '>= 4.7.1'
 gem 'aasm', '~> 5.0', '>= 5.0.6'
+gem 'friendly_id', '~> 5.3'
+gem 'babosa', '~> 1.0', '>= 1.0.3'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,7 @@
 class Story < ApplicationRecord
+  extend FriendlyId
+  friendly_id :slug_candidate, use: :slugged
+
   include AASM
 
   belongs_to :user
@@ -21,5 +24,17 @@ class Story < ApplicationRecord
     event :unpublish do
       transitions from: :published, to: :draft
     end
+  end
+
+  def normalize_friendly_id(input)
+    input.to_s.to_slug.normalize(transliterations: :russian).to_s
+  end
+
+  private
+  def slug_candidate
+    [
+      :title,
+      [:title, SecureRandom.hex[0, 8]]
+    ]
   end
 end
